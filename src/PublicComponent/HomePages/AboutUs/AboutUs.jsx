930f6e8c13d060c8e6c32cbbe0782a24d57d436a
naim0018 <mdkazinaim0018@gmail.com>
2023-12-14 23:07:04 +0600

    add dark mode css

--- src/PublicComponent/HomePages/AboutUs/AboutUs.jsx
+++ src/PublicComponent/HomePages/AboutUs/AboutUs.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
     <div className="relative container mx-auto">
       <img className="w-full h-full object-cover" src={bgimg} alt="Background" />
       <div className=" absolute inset-0 flex items-center justify-center text-center bg-black bg-opacity-50 ">
-        <div className='bg-white w-4/5 px-14 py-20 text-black'>
+        <div className='bg-white w-4/5 px-14 py-20 text-black dark:bg-black dark:bg-opacity-20 dark:text-white dark:text-opacity-80'>
           <h2 className="text-5xl font-bold mb-4">Bistro Bosss</h2>
           <p className='w-3/4 mx-auto'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ex laboriosam
